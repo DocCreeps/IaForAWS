a5348ed5f516b6fadeb93778c3c1338d66f2880b
DocCreeps <dorian.vericel0546@gmail.com>
2021-04-27 12:25:23 +0200

    add import

--- reconnaissanceImage.py
+++ reconnaissanceImage.py
@@ -4,4 +4,15 @@ Created on Mon Apr 26 16:07:46 2021
 
 @author: dorian, alexandre
 """
+#import des lib
 
+import cv2
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import datetime
+from tensorflow import keras
+from tensorflow.keras.models import Model
+import tensorflow as tf
