81579fcc84c78a63aee04f6a751e71f395cbd9b6
DocCreeps <dorian.vericel0546@gmail.com>
2021-04-28 10:21:22 +0200

    test

--- reconnaissanceImage.py
+++ reconnaissanceImage.py
@@ -4,7 +4,7 @@ Created on Mon Apr 26 16:07:46 2021
 
 @author: dorian, alexandre
 """
-#import des lib
+# import des lib
 
 import cv2
 import numpy as np
@@ -16,3 +16,22 @@ import datetime
 from tensorflow import keras
 from tensorflow.keras.models import Model
 import tensorflow as tf
+
+url_pikachu = r'pikachu.png?raw=true'
+resp = requests.get(url_pikachu, stream=True).raw
+image_array_pikachu = np.asarray(bytearray(resp.read()), dtype="uint8")
+print(f'Shape of the image {image_array_pikachu.shape}')
+image_pikachu = cv2.imdecode(image_array_pikachu, cv2.IMREAD_COLOR)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(image_pikachu, cv2.COLOR_BGR2RGB))
+plt.show()
+
+url_rondoudou = r'rondoudou.png?raw=true'
+resp = requests.get(url_rondoudou, stream=True).raw
+image_array_rondoudou = np.asarray(bytearray(resp.read()), dtype="uint8")
+print(f'Shape of the image {image_array_rondoudou.shape}')
+image_rondoudou = cv2.imdecode(image_array_rondoudou, cv2.IMREAD_COLOR)
+plt.axis('off')
+plt.imshow(cv2.cvtColor(image_rondoudou, cv2.COLOR_BGR2RGB))
+plt.show()
+
